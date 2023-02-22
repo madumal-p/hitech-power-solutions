0bf5c7d254685ae02a10cc231965564ad8714e68
madumal-p <99251809+madumal-p@users.noreply.github.com>
2023-02-23 00:03:32 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -8,30 +10,29 @@ module.exports = {
       colors: {
         primary: "#EE2626",
         secondary: "#221F1E",
-        primaryred:"#ED2726",
+        grey: "#F0F0F0",
+        primaryred: "#ED2726",
       },
       fontFamily: {
-        Hind: ["Hind", "sans-serif"],
-        DMSans: ["DM Sans", "sans-serif"],
-       },
+        sans: ["var(--font-dm-sans)", ...fontFamily.sans],
+      },
       fontSize: {
-        'sizeone': '1rem',
-        'lgheading': '1.4rem',
-        'pointtwentyfour': '1.5rem',
-        'blackheading':'2.6rem',
-        'smallheading':'1.3rem',
-        'bodyfont':'1.125rem',
-        'pointnine':'0.9rem',
-        'pointeight':'0.8rem',
-        'pointnineteen':'1.188rem',
-        'pointeighteen':'1.125rem',
-
+        sizeone: "1rem",
+        lgheading: "1.4rem",
+        pointtwentyfour: "1.5rem",
+        blackheading: "2.6rem",
+        smallheading: "1.3rem",
+        bodyfont: "1.125rem",
+        pointnine: "0.9rem",
+        pointeight: "0.8rem",
+        pointnineteen: "1.188rem",
+        pointeighteen: "1.125rem",
       },
       fontWeight: {
         light: 300,
         semibold: 600,
         bold: 700,
-        extrabold:800,
+        extrabold: 800,
       },
     },
   },
