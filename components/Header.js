07a4228681dabcd26d27fe99cd4c78c0366f2291
madumal-p <99251809+madumal-p@users.noreply.github.com>
2023-02-22 23:50:38 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -6,12 +6,14 @@ import {
   AiFillInstagram,
   AiFillYoutube,
   AiFillTwitterCircle,
+  AiOutlineShoppingCart,
   AiFillPhone,
   AiOutlineMail,
 } from "react-icons/ai";
 import Image from "next/image";
 import styles from "../styles/Header.module.css";
 import { FaBars } from "react-icons/fa";
+import SearchBar from "../components/SearchBar";
 
 function Header() {
   const router = useRouter();
@@ -19,52 +21,47 @@ function Header() {
   return (
     <header>
       <div
-        className={`appContainer bg-white py-5 px-10  block md:flex justify-between`}
+        className={`container mx-auto bg-white py-3 px-10  block md:flex justify-between`}
       >
-        <div
-          className={`${styles.logobox} w-full  text-center md:text-left md:w-2/5`}
-        >
+        <div className={`${styles.logobox} w-2/12`}>
           <Link href="/">
-            <a>
-              <Image
-                src="/hitech-power-logo.png"
-                alt="logo"
-                width={280}
-                height={110}
-              />
-            </a>
+            <Image
+              src="/hitech-power-logo.png"
+              alt="logo"
+              width={220}
+              height={80}
+            />
           </Link>
         </div>
-        <div className="hidden md:flex justify-between w-3/5">
-          <a className="flex items-center" href="tel: +61 1111 98393">
-            <AiFillPhone size={32} color="red" />
-            <div className="ml-3">
-              <h3 className="text-pointnine lg:text-xl font-DMSans font-medium">
-                Phone Number
-              </h3>
-              <p className="text-pointeight lg:text-lg">+61 1111 98393</p>
+        <div className="hidden md:flex justify-between items-center w-10/12 ml-8">
+          <div className="w-8/12 mr-4 h-10 ">
+            <SearchBar />
+          </div>
+          <a className="w-3/12 ml-6" href="tel: +61 1111 98393">
+            <div className="flex items-center font-sans font-medium">
+              <AiFillPhone size={32} color="red" />{" "}
+              <span className="ml-2 ">Phone Number</span>
+            </div>
+            <div className="block ml-2 text-md font-semibold">
+              +61 1111 98393
             </div>
           </a>
-          <a
-            className="flex items-center"
-            href="mailto:info@hitechpowersolutions.com.au"
-          >
-            <AiOutlineMail size={32} color="red" />
-            <div className="ml-3">
-              <h3 className="text-pointnine lg:text-xl font-DMSans font-medium">
-                Send Your Email
-              </h3>
-              <p className="text-pointeight lg:text-lg">
-                info@hitechpowersolutions.com.au
-              </p>
+
+          <a className="w-3/12" href="mailto:info@hitechpowersolutions.com.au">
+            <div className="flex items-center font-sans font-medium">
+              <AiOutlineMail size={32} color="red" />{" "}
+              <span className="ml-2 ">Send Your Email</span>
+            </div>
+            <div className="block ml-0 text-md font-semibold">
+              info@hitechpowersolutions.com.au
             </div>
           </a>
         </div>
       </div>
       <div className="bg-secondary">
-        <nav className="appContainer relative flex flex-wrap items-center text-white justify-between mb-3">
+        <nav className="container mx-auto relative flex flex-wrap items-center text-white justify-between">
           <div className="w-full mx-auto flex flex-row-reverse flex-wrap items-center justify-between">
-            <div className="w-full relative flex flex-row-reverse lg:w-auto lg:static lg:flex lg:justify-start py-4">
+            <div className="w-full relative flex flex-row-reverse lg:w-auto lg:static lg:flex lg:justify-start py-3">
               <a
                 href="https://www.instagram.com/"
                 target="_blank"
@@ -122,47 +119,43 @@ function Header() {
             >
               <ul className="flex grow flex-col lg:flex-row list-none">
                 <li className="nav-item">
-                  <Link href="/">
-                    <a
-                      className={`${
-                        router.pathname == "/" && styles.active
-                      } w-full lg:w-auto inline-block py-5 px-5 text-base font-bold uppercase hover:bg-primary font-Hind`}
-                    >
-                      Home
-                    </a>
+                  <Link
+                    href="/"
+                    className={`${
+                      router.pathname == "/" && styles.active
+                    } w-full lg:w-auto inline-block py-3 px-6 text-base font-bold uppercase hover:bg-primary font-sans`}
+                  >
+                    Home
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link href="/products">
-                    <a
-                      className={`${
-                        router.pathname == "/products" && styles.active
-                      } w-full lg:w-auto inline-block py-5 px-5 text-base font-bold uppercase hover:bg-primary font-Hind`}
-                    >
-                      Products
-                    </a>
+                  <Link
+                    href="/products"
+                    className={`${
+                      router.pathname == "/products" && styles.active
+                    } w-full lg:w-auto inline-block py-3 px-6 text-base font-bold uppercase hover:bg-primary font-sans`}
+                  >
+                    Products
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link href="/about">
-                    <a
-                      className={`${
-                        router.pathname == "/about" && styles.active
-                      } w-full lg:w-auto inline-block py-5 px-5 text-base font-bold uppercase hover:bg-primary font-Hind`}
-                    >
-                      About Us
-                    </a>
+                  <Link
+                    href="/about"
+                    className={`${
+                      router.pathname == "/about" && styles.active
+                    } w-full lg:w-auto inline-block py-3 px-6 text-base font-bold uppercase hover:bg-primary font-sans`}
+                  >
+                    About Us
                   </Link>
                 </li>
                 <li>
-                  <Link href="/contact">
-                    <a
-                      className={`${
-                        router.pathname == "/contact" && styles.active
-                      } w-full lg:w-auto inline-block py-5 px-5 text-base font-bold uppercase hover:bg-primary font-Hind`}
-                    >
-                      Contact
-                    </a>
+                  <Link
+                    href="/contact"
+                    className={`${
+                      router.pathname == "/contact" && styles.active
+                    } w-full lg:w-auto inline-block py-3 px-6 text-base font-bold uppercase hover:bg-primary font-sans`}
+                  >
+                    Contact
                   </Link>
                 </li>
               </ul>
