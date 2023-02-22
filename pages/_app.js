7b8e8564ab804e49faff4c25d277460b70fcf08d
madumal-p <99251809+madumal-p@users.noreply.github.com>
2023-02-22 23:55:53 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,27 @@
-import "../styles/globals.css";
-import "swiper/scss";
+import "../styles/tailwind.css";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
 import "react-image-gallery/styles/css/image-gallery.css";
-import "swiper/scss/navigation";
-import "swiper/scss/pagination";
 import ShopProvider from "../context/shopContext";
 import { useRouter } from "next/router";
+import { DM_Sans } from "@next/font/google";
+
+const dm_Sans = DM_Sans({
+  weight: ["400", "500", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+  variable: "--font-dm-sans",
+});
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
     <ShopProvider>
-      <Component {...pageProps} key={router.asPath} />
+      <main className={`${dm_Sans.className} ${dm_Sans.variable}`}>
+        <Component {...pageProps} key={router.asPath} />
+      </main>
     </ShopProvider>
   );
 }
