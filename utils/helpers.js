17257447968a3c207583b4fad12238cb9ac76264
madumal-p <99251809+madumal-p@users.noreply.github.com>
2023-02-22 23:56:57 +0530

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,3 +3,20 @@ export const formatter = new Intl.NumberFormat("en-AU", {
   currency: "AUD",
   minimumFractionDigits: 2,
 });
+
+export function textTruncate(
+  str,
+  maxLength,
+  { side = "end", ellipsis = "..." } = {}
+) {
+  if (str.length > maxLength) {
+    switch (side) {
+      case "start":
+        return ellipsis + str.slice(-(maxLength - ellipsis.length));
+      case "end":
+      default:
+        return str.slice(0, maxLength - ellipsis.length) + ellipsis;
+    }
+  }
+  return str;
+}
